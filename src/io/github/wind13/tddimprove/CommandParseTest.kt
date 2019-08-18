211e4ca3acf9e1aca3d074dc38c94060c93351a0
Simon Jun Sheng Liu <wind13@163.com>
2019-08-18 13:00:52 +0800

    First commit

--- src/io/github/wind13/tddimprove/CommandParseTest.kt
+++ src/io/github/wind13/tddimprove/CommandParseTest.kt
@@ -7,18 +7,26 @@ object CommandParseTest {
 
     @Test
     fun test_param_create(): Unit {
-        val p1:Param = Param("l  ")
+        val p1: Param = Param("l  ")
         assertEquals("l", p1.getLabel())
         assertEquals("", p1.getValue())
-        val p2:Param = Param("p 8080  ")
+        val p2: Param = Param("p 8080  ")
         assertEquals("p", p2.getLabel())
         assertEquals("8080", p2.getValue())
     }
+
     @Test
     fun test_command_parser_input(): Unit {
         val commandLine: String = "java -jar server.jar -l -p 8080 -d /usr/logs"
-        val command:Command = Command(commandLine)
+        val command: Command = Command(commandLine)
         assertEquals(commandLine, command.commandLine)
     }
 
+    @Test
+    fun test_command_parse_output(): Unit {
+        val commandLine: String = "java -jar server.jar -l -p 8080 -d /usr/logs"
+        val command: Command = Command(commandLine)
+//        assertEquals(true, command.getValue("l"))
+        assertEquals(9, 3*3)
+    }
 }

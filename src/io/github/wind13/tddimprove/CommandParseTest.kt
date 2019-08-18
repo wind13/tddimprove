2fa47e0c2f24a0f18996fab3888122ad29447cc8
Simon Jun Sheng Liu <wind13@163.com>
2019-08-18 11:52:16 +0800

    First commit

--- src/io/github/wind13/tddimprove/CommandParseTest.kt
+++ src/io/github/wind13/tddimprove/CommandParseTest.kt
@@ -9,6 +9,7 @@ object CommandParseTest {
     fun test_command_parser_input(): Unit {
         val commandLine: String = "java -jar server.jar -l -p 8080 -d /usr/logs"
         val command:Command = Command(commandLine)
+        assertEquals(commandLine, command.commandLine)
         assertEquals(9, 3*3)
     }
 

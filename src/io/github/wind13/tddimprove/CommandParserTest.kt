4ac8e273d87bcbe3e98fb399272ddca45d1a73e5
Simon Jun Sheng Liu <wind13@163.com>
2019-08-18 11:37:35 +0800

    First commit

---
+++ src/io/github/wind13/tddimprove/CommandParserTest.kt
@@ -0,0 +1,13 @@
+package io.github.wind13.tddimprove
+
+import org.testng.annotations.Test
+import kotlin.test.assertEquals
+
+object CommandParserTest {
+
+    @Test
+    fun test_command_parser_input(): Unit {
+        assertEquals(9, 3*3)
+    }
+
+}

60b2bf04d6df50909c032211394a0ad0a883eb63
Simon Jun Sheng Liu <wind13@163.com>
2019-08-20 16:01:26 +0800

    First commit

--- src/io/github/wind13/tddimprove/args03/Commander.kt
+++ src/io/github/wind13/tddimprove/args03/Commander.kt
@@ -2,5 +2,6 @@ package io.github.wind13.tddimprove.args03
 
 class Commander(val line: String) {
 
+    val command: String = line.split("-").get(0).trim()
     val params: List<Param> = line.split("-").map { Param(it.trim()) }
 }

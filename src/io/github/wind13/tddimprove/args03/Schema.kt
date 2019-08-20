d08144d25c1c91b3b77a9063390337e31612fc4e
Simon Jun Sheng Liu <wind13@163.com>
2019-08-20 15:31:28 +0800

    First commit

--- src/io/github/wind13/tddimprove/args03/Schema.kt
+++ src/io/github/wind13/tddimprove/args03/Schema.kt
@@ -1,12 +1,14 @@
 package io.github.wind13.tddimprove.args03
 
 class Schema(val input: String) {
+    private fun find(label: String) = specs.find { it.label == label }
+
     fun type(label: String): String {
-        return specs.find { it.label == label }?.type ?: ""
+        return find(label)?.type ?: ""
     }
 
-    fun default(input: String): Int {
-        return 8050
+    fun default(label: String): Any {
+        return find(label)?.default?:""
     }
 
     val specs: List<Spec> = input.split("|").map { Spec(it) }

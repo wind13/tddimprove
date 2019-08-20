0479415e194f846fecddd47c07bab77845b2bc14
Simon Jun Sheng Liu <wind13@163.com>
2019-08-20 14:45:01 +0800

    First commit

--- src/io/github/wind13/tddimprove/args03/SpecTest.kt
+++ src/io/github/wind13/tddimprove/args03/SpecTest.kt
@@ -1,11 +1,15 @@
 package io.github.wind13.tddimprove.args03
 
 import org.testng.annotations.Test
+import kotlin.test.assertEquals
 
 object SpecTest {
 
     @Test
     fun test_create_spec(): Unit {
-        val spec:Spec = Spec("l:bool:true")
+
+        val input = "l:bool:true"
+        val spec:Spec = Spec(input)
+        assertEquals(input, spec.input)
     }
 }

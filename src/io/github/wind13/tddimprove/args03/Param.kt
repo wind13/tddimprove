d72922f712568713ee040919498d2de6305c3b4a
Simon Jun Sheng Liu <wind13@163.com>
2019-08-20 15:49:55 +0800

    First commit

--- src/io/github/wind13/tddimprove/args03/Param.kt
+++ src/io/github/wind13/tddimprove/args03/Param.kt
@@ -2,7 +2,11 @@ package io.github.wind13.tddimprove.args03
 
 class Param(val input:String) {
     private val regex = Regex("\\s+")
-    private fun trimSplit(n:Int) = input.trim().split(regex)[n].trim()
+    private val split = input.trim().split(regex)
+    private fun trimSplit(n:Int):String {
+
+        return split[n].trim()
+    }
 
     val label: String = trimSplit(0)
     val value: String = trimSplit(1)

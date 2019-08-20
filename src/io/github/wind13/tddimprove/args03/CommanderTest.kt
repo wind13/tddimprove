db4836d40bd736f19d50b4978ac1120d16fab877
Simon Jun Sheng Liu <wind13@163.com>
2019-08-20 16:42:50 +0800

    First commit

--- src/io/github/wind13/tddimprove/args03/CommanderTest.kt
+++ src/io/github/wind13/tddimprove/args03/CommanderTest.kt
@@ -23,9 +23,17 @@ object CommanderTest {
 
     @Test
     fun test_array_commander(): Unit {
-        val input = "l:bool:true|t:str[]:8050|d:str:/usr/logs"
+        val input = "t:str[]:a,b,c"
         val schema: Schema = Schema(input)
         val commander:Commander = schema.parse("java -jar capture.jar -url www.baidu.com -t jpg,png,svg -d /usr/images")
         assert(commander.toString().contains("Commander"))
+        assertEquals(5, commander.comlines.size)
+        assertEquals(4, commander.params.size)
+        assertEquals("java", commander.command)
+        assertEquals("www.baidu.com", commander.param("url"))
+        assertEquals("capture.jar", commander.param("jar"))
+        assertEquals(listOf("jpg", "png", "svg"), commander.param("t"))
+//        assertEquals(8010, commander.param("p"))
+        assertEquals("/usr/images", commander.param("d"))
     }
 }

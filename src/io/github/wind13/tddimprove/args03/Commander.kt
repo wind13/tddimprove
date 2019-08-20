efce3cd5eee886a58ca4beac8bd52d7a7a021ffc
Simon Jun Sheng Liu <wind13@163.com>
2019-08-20 16:30:49 +0800

    First commit

--- src/io/github/wind13/tddimprove/args03/Commander.kt
+++ src/io/github/wind13/tddimprove/args03/Commander.kt
@@ -1,17 +1,16 @@
 package io.github.wind13.tddimprove.args03
 
 class Commander(val schema: Schema, val line: String) {
-    fun param(label: String): Any {
-        val value = params.find { it.label == label }?.value ?: ""
-        return if (value == "") default(label) else Parser.parse(schema.type(label), value)
-    }
+    val comlines = line.split("-")
+    val command: String = comlines.first().trim()
+    val params: List<Param> = comlines.map { Param(it.trim()) }
 
     fun default(label: String): Any {
         return schema.default(label)
     }
 
-    val comlines = line.split("-")
-
-    val command: String = comlines.get(0).trim()
-    val params: List<Param> = comlines.map { Param(it.trim()) }
+    fun param(label: String): Any {
+        val value = params.find { it.label == label }?.value ?: ""
+        return if (value == "") default(label) else Parser.parse(schema.type(label), value)
+    }
 }

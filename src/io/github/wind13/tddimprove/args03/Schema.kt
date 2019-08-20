e866aecc65756a9cb796231a84b0a3a156139934
Simon Jun Sheng Liu <wind13@163.com>
2019-08-20 15:16:36 +0800

    First commit

---
+++ src/io/github/wind13/tddimprove/args03/Schema.kt
@@ -0,0 +1,5 @@
+package io.github.wind13.tddimprove.args03
+
+class Schema(val input: String) {
+
+}
---
+++ src/io/github/wind13/tddimprove/args03/SchemaTest.kt
@@ -0,0 +1,11 @@
+package io.github.wind13.tddimprove.args03
+
+import org.testng.annotations.Test
+
+object SchemaTest {
+
+    @Test
+    fun test_create_schema(): Unit {
+        val schema:Schema = Schema("")
+    }
+}

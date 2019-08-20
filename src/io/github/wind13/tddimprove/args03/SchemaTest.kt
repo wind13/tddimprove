6271eb9871ee1e9ae3460886f72e4ddab6a3c71a
Simon Jun Sheng Liu <wind13@163.com>
2019-08-20 15:18:00 +0800

    First commit

--- src/io/github/wind13/tddimprove/args03/SchemaTest.kt
+++ src/io/github/wind13/tddimprove/args03/SchemaTest.kt
@@ -1,11 +1,14 @@
 package io.github.wind13.tddimprove.args03
 
 import org.testng.annotations.Test
+import kotlin.test.assertEquals
 
 object SchemaTest {
 
     @Test
     fun test_create_schema(): Unit {
-        val schema:Schema = Schema("")
+        val input = "l:bool:true|p:int:8050|d:str:/usr/logs"
+        val schema:Schema = Schema(input)
+        assertEquals(input, schema.input)
     }
 }

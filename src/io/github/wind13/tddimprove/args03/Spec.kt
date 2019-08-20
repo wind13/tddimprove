78857e6c472389f5d5bff47985b1380148bb95a0
Simon Jun Sheng Liu <wind13@163.com>
2019-08-20 14:49:08 +0800

    First commit

--- src/io/github/wind13/tddimprove/args03/Spec.kt
+++ src/io/github/wind13/tddimprove/args03/Spec.kt
@@ -1,7 +1,9 @@
 package io.github.wind13.tddimprove.args03
 
 class Spec(val input:String) {
-    val label: String = input.split(":")[0].trim()
-    val type: String = input.split(":")[1].trim()
-    val default: String = input.split(":")[2].trim()
+    private fun nth(n:Int) = input.split(":")[n].trim()
+
+    val label: String = nth(0)
+    val type: String = nth(1)
+    val default: String = nth(2)
 }

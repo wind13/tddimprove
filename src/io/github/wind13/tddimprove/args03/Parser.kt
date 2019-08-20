ad2c2fc2316fab677e91b28e7717cf81c363051a
Simon Jun Sheng Liu <wind13@163.com>
2019-08-20 14:25:41 +0800

    First commit

--- src/io/github/wind13/tddimprove/args03/Parser.kt
+++ src/io/github/wind13/tddimprove/args03/Parser.kt
@@ -1,21 +1,19 @@
 package io.github.wind13.tddimprove.args03
 
 class Parser(val type: String) {
+
+    private fun trimToStr(it: String) = it.trim()
+    private fun trimToBool(it: String) = it.trim().toBoolean()
+    private fun trimToInt(it: String) = it.trim().toInt()
+
     fun parse(strValue: String): Any {
         return when (type) {
             "int" -> trimToInt(strValue)
             "bool" -> trimToBool(strValue)
-            "int[]" -> strValue.split(",").map { trimToInt(it) }
+            "int[]" -> strValue.split(",").map{ trimToInt(it) }
             "bool[]" -> strValue.split(",").map { trimToBool(it) }
             "str[]" -> strValue.split(",").map { trimToStr(it) }
             else -> trimToStr(strValue)
         }
     }
-
-    private fun trimToStr(it: String) = it.trim()
-
-    private fun trimToBool(it: String) = it.trim().toBoolean()
-
-    private fun trimToInt(it: String) = it.trim().toInt()
-
 }

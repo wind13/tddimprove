76858ab18f6b93fc672b948b85057bc0d3e77f44
Simon Jun Sheng Liu <wind13@163.com>
2019-08-20 13:57:16 +0800

    First commit

--- src/io/github/wind13/tddimprove/args03/Parser.kt
+++ src/io/github/wind13/tddimprove/args03/Parser.kt
@@ -1,13 +1,11 @@
 package io.github.wind13.tddimprove.args03
 
-import java.lang.Integer.parseInt
-
 class Parser(val type: String) {
     fun parse(v: String): Any {
         if(type == "int") {
-            return parseInt(v)
+            return v.toInt()
         }
-        return v == "true"
+        return v.toBoolean()
     }
 
 }

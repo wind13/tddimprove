2719d0e6d69fc229f153d2071cc8376ad52f9e77
Simon Jun Sheng Liu <wind13@163.com>
2019-08-20 14:41:39 +0800

    First commit

---
+++ src/io/github/wind13/tddimprove/args03/SpecTest.kt
@@ -0,0 +1,11 @@
+package io.github.wind13.tddimprove.args03
+
+import org.testng.annotations.Test
+
+object SpecTest {
+
+    @Test
+    fun test_create_spec(): Unit {
+//        assert(Spec("l:bool:true"))
+    }
+}

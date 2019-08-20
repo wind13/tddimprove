0f30c0c246ab927a8046422d410409a95ea0ee71
Simon Jun Sheng Liu <wind13@163.com>
2019-08-20 13:45:32 +0800

    First commit

---
+++ src/io/github/wind13/tddimprove/args03/ParserTest.kt
@@ -0,0 +1,12 @@
+package io.github.wind13.tddimprove.args03
+
+import org.testng.annotations.Test
+import kotlin.test.assertEquals
+
+object ParserTest {
+
+    @Test
+    fun test_create_parser(): Unit {
+        assertEquals(9, 3*3)
+    }
+}

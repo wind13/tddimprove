b2a2e093c45e85c5726e27f9f5d0d158873ae432
Simon Jun Sheng Liu <wind13@163.com>
2019-08-21 15:35:56 +0800

    First commit

--- src/io/github/wind13/tddimprove/money/Franc.kt
+++ src/io/github/wind13/tddimprove/money/Franc.kt
@@ -1,21 +1,4 @@
 package io.github.wind13.tddimprove.money
 
-class Franc(var amount: Int) {
-    fun times(multiplier: Int): Franc {
-        return Franc(amount * multiplier)
-    }
+class Franc(amount: Int) : Money(amount)
 
-    override fun equals(other: Any?): Boolean {
-        return when (other) {
-            !is Franc -> false
-            else -> this === other ||
-                    amount == other.amount
-        }
-    }
-
-    override fun hashCode(): Int {
-        var result = 17
-        result = result * 31 + amount.hashCode()
-        return result
-    }
-}

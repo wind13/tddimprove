eb39a859ba96eb41de7159d4cef940116ef463be
Simon Jun Sheng Liu <wind13@163.com>
2019-08-21 12:14:55 +0800

    First commit

--- src/io/github/wind13/tddimprove/money/MoneyTest.kt
+++ src/io/github/wind13/tddimprove/money/MoneyTest.kt
@@ -7,10 +7,12 @@ object MoneyTest {
 
     @Test
     fun test_multiplication(): Unit {
-        val five:Dollar = Dollar(5)
+        val five = Dollar(5)
         assertEquals(5, five.amount)
-        assertEquals(10, five.times(2).amount)
-        assertEquals(15, five.times(3).amount)
         assertEquals(Dollar(5), Dollar(5))
+        val product = five.times(2)
+        assertEquals(Dollar(10), product)
+        val fifteen = five.times(3)
+        assertEquals(Dollar(15), fifteen)
     }
 }

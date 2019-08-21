57ed174cc76b53e92217572f0cc2b15672ef3949
Simon Jun Sheng Liu <wind13@163.com>
2019-08-21 15:38:39 +0800

    First commit

--- src/io/github/wind13/tddimprove/money/MoneyTest.kt
+++ src/io/github/wind13/tddimprove/money/MoneyTest.kt
@@ -2,6 +2,7 @@ package io.github.wind13.tddimprove.money
 
 import org.testng.annotations.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertNotEquals
 
 object MoneyTest {
 
@@ -10,6 +11,7 @@ object MoneyTest {
         val five = Dollar(5)
         assertEquals(5, five.amount)
         assertEquals(Dollar(5), Dollar(5))
+        assertNotEquals(Dollar(5), Dollar(6))
         val product = five.times(2)
         assertEquals(Dollar(10), product)
         val fifteen = five.times(3)
@@ -21,6 +23,7 @@ object MoneyTest {
         val five = Franc(5)
         assertEquals(5, five.amount)
         assertEquals(Franc(5), Franc(5))
+        assertNotEquals(Franc(6), Franc(5))
         val product = five.times(2)
         assertEquals(Franc(10), product)
         val fifteen = five.times(3)

78c8d76e94565c275c14fde817b258da9f749a8c
Simon Jun Sheng Liu <wind13@163.com>
2019-08-24 13:03:48 +0800

    First commit

--- src/io/github/wind13/tddimprove/marsrover/MarsTest.kt
+++ src/io/github/wind13/tddimprove/marsrover/MarsTest.kt
@@ -3,7 +3,9 @@ package io.github.wind13.tddimprove.marsrover
 import org.testng.annotations.Test
 import kotlin.test.assertEquals
 
-@Test
-fun test_normal_fun(): Unit {
-    assertEquals(11, add(3, 8))
+object MarsTest {
+    @Test
+    fun test_normal_fun(): Unit {
+        assertEquals(11, add(3, 8))
+    }
 }

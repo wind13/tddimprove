07f53559b86a5809f1be6a468553bdc3506fdadc
Simon Jun Sheng Liu <wind13@163.com>
2019-08-24 16:34:06 +0800

    First commit

--- src/io/github/wind13/tddimprove/marsrover/Rover.kt
+++ src/io/github/wind13/tddimprove/marsrover/Rover.kt
@@ -1,9 +1,22 @@
 package io.github.wind13.tddimprove.marsrover
 
-class Rover(state: String) {
-    val ss = state.split("|")
-    val xy = ss.get(0).split(",")
-    val x:Int = xy.get(0).trim().toInt()
-    val y:Int = xy.get(1).trim().toInt()
-    val direction: Direction = Direction.getValue(ss.get(1).last())
+class Rover {
+    var x: Int = 0
+    var y: Int = 0
+    var direction: Direction = Direction.SOUTH
+
+    constructor(x:Int, y:Int, direction: Direction) {
+        this.x = x
+        this.y = y
+        this.direction = direction
+    }
+
+    constructor(state: String) {
+        val ss = state.split("|")
+        val xy = ss.get(0).split(",")
+        this.x = xy.get(0).trim().toInt()
+        this.y = xy.get(1).trim().toInt()
+        this.direction = Direction.getValue(ss.get(1).last())
+    }
+
 }

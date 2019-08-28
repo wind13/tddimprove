6052180ddc3b2b2b3d1114c1054927828ad9974e
Simon Jun Sheng Liu <wind13@163.com>
2019-08-28 15:43:37 +0800

    First commit

--- src/io/github/wind13/tddimprove/marsrover/Rover.kt
+++ src/io/github/wind13/tddimprove/marsrover/Rover.kt
@@ -92,10 +92,9 @@ class Rover(val speed: Int) {
     }
 
     fun nCommands(n: Int): String {
-        val a = (1 .. n).map { this.commandUnit }.joinToString { "" }
-        println(a)
-        return this.commandUnit + this.commandUnit +
-                this.commandUnit + this.commandUnit + this.commandUnit
+        return (1 .. n).map { this.commandUnit }.joinToString("")
+//        return this.commandUnit + this.commandUnit +
+//                this.commandUnit + this.commandUnit + this.commandUnit
     }
 
     lateinit var state: String
